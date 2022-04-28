abc3a4c3dfdb6e5fe40803f1e666a0577ad8095b
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-28 16:22:28 +0200

    Remove CompanyTestMixin

--- tests/test_module.py
+++ tests/test_module.py
@@ -2,13 +2,13 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 
-from trytond.modules.company.tests import CompanyTestMixin
 from trytond.tests.test_tryton import ModuleTestCase
 
 
-class ShineTestCase(CompanyTestMixin, ModuleTestCase):
+class ShineTestCase(ModuleTestCase):
     'Test Shine module'
     module = 'shine'
+    extras = ['dashboard']
 
 
 del ModuleTestCase
