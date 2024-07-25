4636388c96d4bd28f44da17946c17b6675f75c49
Jose Arturo Garcia <jose.arturo.garcia@gmail.com>
2024-07-25 08:09:59 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def get_require_version(name):
     return require
 
 config = ConfigParser()
-config.readfp(open('tryton.cfg'))
+config.read_file(open(os.path.join(os.path.dirname(__file__), 'tryton.cfg')))
 info = dict(config.items('tryton'))
 for key in ('depends', 'extras_depend', 'xml'):
     if key in info:
